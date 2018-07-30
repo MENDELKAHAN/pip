46a71af2f4bcdf19ae7ef3bd72b63f4aa328496f
Mendel Kahan <mendy@famkahan.com>
2018-07-30 22:11:21 +0100

    s

---
+++ application/views/login.php
@@ -0,0 +1,30 @@
+<?php include('header.php'); ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Login</h1>
+
+
+				<form action="">
+					<div class="form-group">
+						<!-- <label for="user">User</label> -->
+						<input type="text" class="form-control" id="user" aria-describedby="user" placeholder="User Name">
+					</div>
+
+ 					<div class="form-group">
+						<!-- <label for="password">Password</label> -->
+						<input type="password" class="form-control" id="password" aria-describedby="user" placeholder="Password">
+					</div>
+
+				  <button type="submit" id="submit" class="btn btn-primary btn-block ">Submit</button>
+				</form>
+				 <button  class="btn btn-block ">Create your Account</button>
+
+				
+        		
+			</div>
+		</div> <!-- end row -->
+	</div> <!-- end container -->
+
+
+<?php include('footer.php'); ?>
