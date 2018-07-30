4dc3801e71d7439b17ebcc3c592f2509756264f8
DESKTOP-KIA5S54\mendy <mendy@famkahan.com>
2018-07-30 17:03:28 +0100

    s

---
+++ application/views/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    
+    <title>Welcome to PIP</title>
+    <meta name="description" content="ca test">
+    <meta name="author" content="Mendel Kahan">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <!-- Bootstrap css link -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>static/css/style.css" type="text/css" media="screen" />
+</head>
+<body>
---
+++ application/views/main_view.php
@@ -0,0 +1,29 @@
+<?php include('header.php'); ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Welcome to PIP</h1>
+
+
+				<form action="">
+					<div class="form-group">
+						<label for="user">user</label>
+						<input type="text" class="form-control" id="user" aria-describedby="user" placeholder="User Name">
+					</div>
+
+ 					<div class="form-group">
+						<label for="password">Password</label>
+						<input type="password" class="form-control" id="password" aria-describedby="user" placeholder="Password">
+					</div>
+
+				  <button type="submit" id="submit" class="btn btn-primary btn-block ">Submit</button>
+				</form>
+
+				
+        		
+			</div>
+		</div> <!-- end row -->
+	</div> <!-- end container -->
+
+
+<?php include('footer.php'); ?>
