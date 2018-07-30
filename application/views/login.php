8577cd1daffe60242947d01b4184156edc3a57e1
DESKTOP-KIA5S54\mendy <mendy@famkahan.com>
2018-07-30 22:24:38 +0100

    login form

--- application/views/login.php
+++ application/views/login.php
@@ -1,7 +1,8 @@
 <?php include('header.php'); ?>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-12">
+			<div class="col-md-3"></div>
+			<div class="col-md-6">
 				<h1>Login</h1>
 
 
@@ -16,13 +17,13 @@
 						<input type="password" class="form-control" id="password" aria-describedby="user" placeholder="Password">
 					</div>
 
-				  <button type="submit" id="submit" class="btn btn-primary btn-block ">Submit</button>
+				  <button type="submit" id="submit" class="btn btn-primary btn-block">Submit</button>
 				</form>
-				 <button  class="btn btn-block ">Create your Account</button>
-
-				
+				<a href="register" class="btn register btn-block">Create your Account</a>
+						
         		
 			</div>
+			<div class="col-md-3"></div>
 		</div> <!-- end row -->
 	</div> <!-- end container -->
 
